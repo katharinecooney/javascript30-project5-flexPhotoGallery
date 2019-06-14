458a53e3b064f984c9eb42e7a0b6d840f1c26f41
Katharine Cooney <katharinecooney@gmail.com>
2019-06-14 11:23:38 +0200

    Initialize commit / finalized project

---
+++ index.js
@@ -0,0 +1,22 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  console.log('hello');
+  this.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  console.log(e.propertyName);
+  if(e.propertyName === 'flex' || e.propertyName === 'flex-grow') {
+    this.classList.toggle('open-active');
+  }
+  
+}
+
+panels.forEach(panel => {
+  panel.addEventListener('click', toggleOpen);
+});
+
+panels.forEach(panel => {
+  panel.addEventListener('transitionend', toggleActive);
+});
